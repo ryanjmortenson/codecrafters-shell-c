fc74778993566fb51d69fc3f4558294b5b26f208
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2025-12-02 21:51:14 -0700

    Return command not found

--- src/main.c
+++ src/main.c
@@ -4,10 +4,13 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
+  static char input[1024];
 
   // TODO: Uncomment the code below to pass the first stage
   //
   printf("$ ");
 
+  scanf("%s", input);
+  printf("%s: command not found", input);
   return 0;
 }
