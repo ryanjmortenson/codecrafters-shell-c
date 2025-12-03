a25bc34705d5b8d36dfa2e3e14d5f5c69b7ab528
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2025-12-02 22:06:40 -0700

    Add exit

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
   static char input[1024];
@@ -10,8 +12,21 @@ int main(int argc, char *argv[]) {
   {
     printf("$ ");
 
-    scanf("%s", input);
-    printf("%s: command not found\n", input);
+    char* res = fgets(input, 1024, stdin);
+    if (res != NULL)
+    {
+      int len = strnlen(input, 1024);
+      input[len - 1] = '\0';
+      if (strncmp(input, "exit", 1024) == 0)
+      {
+        break;
+      }
+      printf("%s: command not found\n", input);
+    }
+    else
+    {
+      printf("Couldn't process user input");
+    }
   }
   return 0;
 }
