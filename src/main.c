28c14cc15c1e164e93239011e191624cb6d09c44
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2025-12-03 22:23:38 -0700

    Add hacky echo command
    - Assumes "echo" command is first thing in input
    - Assumes string to echo will always be 1 space after echo command

--- src/main.c
+++ src/main.c
@@ -2,23 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define BUFFER_SIZE (1024)
+
 int main(int argc, char *argv[])
 {
   // Flush after every printf
   setbuf(stdout, NULL);
-  static char input[1024];
+  static char input[BUFFER_SIZE];
 
   while (1)
   {
     printf("$ ");
 
-    char* res = fgets(input, 1024, stdin);
+    char* res = fgets(input, BUFFER_SIZE, stdin);
     if (res != NULL)
     {
-      int len = strnlen(input, 1024);
+      int len = strnlen(input, BUFFER_SIZE);
       input[len - 1] = '\0';
-      if (strncmp(input, "exit", 1024) == 0)
+      if (strncmp(input, "exit", BUFFER_SIZE) == 0)
       {
+        break;
+      }
+      if (strstr(input, "echo") != 0)
+      {
+        printf("%s\n", &input[5]);
         break;
       }
       printf("%s: command not found\n", input);
